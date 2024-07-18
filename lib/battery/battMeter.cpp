10d35a5c39d30ada3f32ce070a7c433c64214d7a
bruno_net <blackhunterdev01@gmail.com>
2024-07-18 14:47:47 -0300

    updating...

--- lib/battery/battMeter.cpp
+++ lib/battery/battMeter.cpp
@@ -1,20 +1,14 @@
-#define BATTERY_PIN A0
-#define R1 100000.00
-#define R2 56000.00
-#define VOLTAGE_MAX 8.4
-#define VOLTAGE_MIN 7.4
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <battMeter.hpp>
 
 void setup()
 {
   //pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop()
 {
-  int sensorValue = analogRead(BATTERY_PIN);
+  int sensorValue = analogRead(A0);
   
   // Calculo do tensão da bateria
   float voltage = sensorValue * (3.3 / 1023.0) * ((R1+R2) / R2);
@@ -34,7 +28,7 @@ void loop()
   Serial.print(batteryLevel);
   Serial.println(" %");
   
-  delay(1000);
+  // delay(1000);
 
 }
 
