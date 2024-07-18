fda9fa6c98cedb67f3e24076616659b279c8b9d6
bruno_net <blackhunterdev01@gmail.com>
2024-07-18 15:45:40 -0300

    UPDATES

--- lib/battery/battMeter.hpp
+++ lib/battery/battMeter.hpp
@@ -1,4 +1,6 @@
-#define BATTERY_PIN A0
+#ifndef BATTMETER_HPP
+#define BATTMETER_HPP
+
 #define R1 25000.00
 #define R2 14800.00
 #define VOLTAGE_MAX 8.4
@@ -6,3 +8,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
+namespace BattMeter {
+    void setup(void);
+}
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,8 @@ void setup() {
 
 	Motor::setup();
 
+	
+
 }
 
 void loop() {
